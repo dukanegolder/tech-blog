06052f6cd9cd76ec298813f13311ebe8822c74f3
dukanegolder <dukanegolder@gmail.com>
2023-03-19 19:17:50 -0600

    created model for bolg.js

--- models/Blog.js
+++ models/Blog.js
@@ -1,2 +1,43 @@
 const { Model, Datatypes } = require("sequelize");
 const sequelize = require("../config/connection");
+
+class Blog extends Model {}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    contents: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    date_created: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    modelName: "blog",
+  }
+);
+
+module.exports = Blog;
